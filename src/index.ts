db4a7271eb9b607c17d61016b876c642084fb7c6
Franks <czfranks@gmail.com>
2024-09-14 16:53:14 -0500

    chore: connect to db

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
 import express from 'express';
+import pg from 'pg';
 
 const app = express();
 
+const pool = new pg.Pool({
+  user: process.env['PG_USER'],
+  password: process.env['PG_PASSWORD'],
+  host: 'localhost',
+  port: 5432,
+  database: 'time_tracker',
+});
+
 app.get('/', (_req, res) => {
   res.send('Hello world');
+});
+
+app.get('/users', async (_req, res) => {
+  const result = await pool.query('select * from users');
+  const users = result.rows;
+  res.json(users);
 });
 
 app.listen(3000, () => {
